4126b12a6755e1025403c23844b93b549851f48c
Matheus Canovas Almeida Santos <msantos@agecomnet.com.br>
2025-05-14 17:51:15 -0300

    uploading the project

---
+++ src/test/java/com/github/mattcanovas/PersonServiceTest.java
@@ -0,0 +1,7 @@
+package com.github.mattcanovas;
+
+import org.junit.jupiter.api.DisplayName;
+
+public class PersonServiceTest {
+    @DisplayName("When Create a Person with SUccess Should Return a Person")
+}
